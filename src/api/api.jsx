46fd2d6ee6811fd869cb787ec8e04912a8ab1a5c
Gokula Kannan S <gokulakannan.sj7@gmail.com>
2025-05-20 22:50:53 +0530

    Update api.jsx

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({ 
-  baseURL: "process.env.REACT_APP_API_URL",
+  baseURL: "https://prog-hub-server.onrender.com/api",
   headers:{"Content-Type":"application/json"}
  });
 
